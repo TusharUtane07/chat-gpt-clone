f64f597e2cf76a18615d22c68c93549b24571dcb
Tushar Utane <89387408+CodeWitTushar@users.noreply.github.com>
2023-02-17 12:25:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import send from "./assests/send.svg";
+import user from "./assests/user.png";
+import loadingIcon from "./assests/loader.svg";
+import bot from "./assests/bot.png";
+import axios from 'axios'
 
-function App() {
+
+// let arr = [
+//     {
+//         type : "user", post: "fsafafaafaf"
+//     },
+//     {
+//         type : "bot", post : "fafafafafaf"
+//     }
+// ]
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect = (() =>{
+    document.querySelector('.layout').scrollTop = document.querySelector('.layout').scrollHeight;
+  },[posts]) 
+
+  const fetchBotResponse = async () => {
+    const {data} = await axios.post("http://localhost:4000", {input}, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }) 
+    return data;
+  }
+
+  const onSubmit = () => {
+    if (input.trim() === "") return;
+    updatePosts(input);
+    updatePosts("Loading...", false, true);
+    setInput("")
+    fetchBotResponse().then((res)=>{
+      console.log(res);
+      updatePosts(res.bot.trim(), true);
+    });
+  }
+
+  const autoTypingBotResponse = (text) => {
+    let index = 0;
+    let interval = setInterval(()=> {
+      if(index < text.length) {
+        setPosts((prevState)=>{
+          let lastItem = prevState.pop();
+          if(lastItem.type !== "bot"){
+            prevState.push({
+              type: "bot",
+              post: text.charAt(index-1)
+            })
+          } else {
+            prevState.push({
+              type: "bot",
+              post: lastItem.post + text.charAt(index-1)
+            })
+          }
+          return [...prevState]
+        })
+        index++;
+      }else{
+        clearInterval(interval);
+      }
+    }, 30)
+  }
+
+  const updatePosts = (post, isBot, isLoading) => {
+    if(isBot) {
+      autoTypingBotResponse(post)
+    }else{ 
+      setPosts(prevState => {
+          return [
+              ...prevState, 
+              {type: isLoading ? "loading" : "user", post}
+          ]
+      })
+    }
+  }
+
+  const onKeyUp = (e) => {
+    if (e.key === "Enter" || e.which === 13){
+        onSubmit()
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="chatGPT-app">
+      <section className="chat-container">
+                <div className="layout">
+                    {posts.map((post, index) => (
+                        <div
+                            key={index}
+                            className={`chat-bubble ${
+                                post.type === "bot" || post.type === "loading"
+                                    ? "bot"
+                                    : ""
+                            }`}
+                        >
+                            <div className="avatar">
+                                <img
+                                    src={
+                                        post.type === "bot" ||
+                                        post.type === "loading"
+                                            ? bot
+                                            : user
+                                    }
+                                />
+                            </div>
+                            {post.type === "loading" ? (
+                                <div className="loader">
+                                    <img src={loadingIcon} />
+                                </div>
+                            ) : (
+                                <div className="post">{post.post}</div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </section>
+      <footer>
+        <input
+          className="composebar"
+          value={input}
+          autoFocus
+          type="text"
+          placeholder="Ask Anything!"
+          onChange={(e) => setInput(e.target.value)}
+          onKeyUp={onKeyUp}
+        />
+        <div className="send-button" onClick={onSubmit}>
+          <img src={send} alt="" />
+        </div>
+      </footer>
+    </main>
   );
-}
+};
 
 export default App;
